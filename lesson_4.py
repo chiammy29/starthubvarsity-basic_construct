13675f8a01cfcbd498c4edcf468dbfd7e18dafcf
chiammy29 <ibenemechiamaka54@gmail.com>
2024-10-02 12:18:58 +0100

    first commit

---
+++ lesson_4.py
@@ -0,0 +1,14 @@
+def amaka_ibe_func():
+    # this is my native name
+    name = "amaka_ibe"
+    # this is not my real age
+    age = 20
+    # i'm definitely a female
+    is_male = False
+    # i'm this tall but some girls are taller 
+    height = 5.7
+    print(name,age,is_male,height)
+
+    
+    amaka_ibe_func()
+    
